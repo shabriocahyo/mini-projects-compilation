045c7e03eef93ecb1bc51c9d4abc4c4fb61d9921
Shabrio Cahyo <84552612+shabriocahyo@users.noreply.github.com>
2022-01-12 14:16:41 +0700

    Update pohon-natal-i.cpp

--- Pohon Natal/C++/pohon-natal-i.cpp
+++ Pohon Natal/C++/pohon-natal-i.cpp
@@ -1,1 +1,39 @@
-// code
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Masukan Tinggi Pohon : ";
+    cin >> x;
+    for(int i = 1; i <= x; i++){
+        for(int j = x; j >= i; j--){
+            cout<<" ";
+        }
+        
+        for(int l = 2; l <= i; l++){
+            cout<<"*";
+        }
+        
+        for(int k = 1; k <= i; k++){
+                cout<<"*";
+            }
+            
+        cout<<endl;
+            
+        if(i==x){
+            for(int m = 0; m < 3; m++){
+                for(int o = 1; o < i; o++){
+                cout<<" ";
+                }
+                
+                for(int n = 0; n <= 0; n++){
+                cout<<"===";
+                }
+                
+            cout<<endl;
+            }
+        }
+    }
+
+    return 0;
+}
